58ecc5260cc92c3008a7af007cb5aa18540f0dc9
Andrea-Giugie <bisateo@gmail.com>
2020-05-20 15:54:50 +0200

    Aggiungere i file di progetto.

---
+++ LabProg2019/Maze.fs
@@ -0,0 +1,161 @@
+﻿(*
+* LabProg2019 - Progetto di Programmazione a.a. 2019-20
+* Maze.fs: maze
+* (C) 2019 Alvise Spano' @ Universita' Ca' Foscari di Venezia
+*)
+
+module LabProg2019.Maze
+
+open External
+open Gfx
+open System
+open System.Text
+
+
+
+[<Diagnostics.DebuggerDisplay("{ ToString ()}")>]
+
+
+type CharInfo with
+    /// Shortcut for creating a wall pixel.
+    static member wall = pixel.create (Config.wall_pixel_char, Color.White)
+    /// Shortcut for creating a path pixel.
+    static member internal path = pixel.filled Color.Black
+    /// Check whether this pixel is a wall.
+    member this.isWall = this = pixel.wall
+
+let getRandomVicino(x:int,y:int,maxX:int,maxY:int,matrice:cell[,])=
+    let mutable i = 0
+    let l =[|
+        if((x-1)>=0 && (x-1)<maxX && y>=0 && y<maxY && (matrice.[x-1,y].visited=false) ) then yield false,(matrice.[x-1,y]);i<-i+1   //Sinistra
+        if((x+1)>=0 && (x+1)<maxX && y>=0 && y<maxY && (matrice.[x+1,y].visited=false)) then yield false,(matrice.[x+1,y]);i<-i+1     //Destra
+        if((x)>=0 && x<maxX && (y-1)>=0 && (y-1)<maxY && (matrice.[x,y-1].visited=false)) then yield false,(matrice.[x,y-1]);i<-i+1     //Sopra
+        if((x)>=0 && x<maxX && (y+1)>=0 && (y+1)<maxY && (matrice.[x,y+1].visited=false)) then yield false,(matrice.[x,y+1]);i<-i+1     //Sotto
+    |]
+    let r = System.Random().Next(0, i)
+    if i = 0 then 
+        (true, (new cell()))
+    else l.[r]
+//La differenza con quello di prima è che mi prende le celle vicine con un percorso(ci sono i muri aperti)
+let getPossiblePath(x:int,y:int,maxX:int,maxY:int,matrice:cell[,])=
+    let mutable i = 0
+    let l =[|
+        if((x-1)>=0 && (x-1)<maxX && y>=0 && y<maxY && (matrice.[x-1,y].visited=false) && matrice.[x,y].topWall=false) then yield false,(matrice.[x-1,y]);i<-i+1   //sopra
+        if((x+1)>=0 && (x+1)<maxX && y>=0 && y<maxY && (matrice.[x+1,y].visited=false) && matrice.[x,y].bottomWall=false ) then yield false,(matrice.[x+1,y]);i<-i+1     //sotto
+        if((x)>=0 && x<maxX && (y-1)>=0 && (y-1)<maxY && (matrice.[x,y-1].visited=false) && matrice.[x,y].leftWall=false) then yield false,(matrice.[x,y-1]);i<-i+1     //sinistra
+        if((x)>=0 && x<maxX && (y+1)>=0 && (y+1)<maxY && (matrice.[x,y+1].visited=false)&& matrice.[x,y].rightWall=false ) then yield false,(matrice.[x,y+1]);i<-i+1     //destra
+    |]
+    let r = System.Random().Next(0, i)
+    if i = 0 then 
+        (true, (new cell()))
+    else l.[r]
+
+let getBool((b,c):bool*cell)=b
+let getCell((b,c):bool*cell)=c
+
+let rec push(l:'a list, elemento:'a)=
+    match l with
+        |[]->[elemento]
+        |[x] -> [x]@[elemento]
+        |x::xs->x::push(xs,elemento)
+
+        
+let cancellaMuri(c1:cell,c2:cell)=
+    let Deltax = c1.x-c2.x
+    let Deltay = c1.y-c2.y
+    if Deltax = 1 then
+        c1.leftWall<-false
+        c2.rightWall<-false
+    if Deltax = -1 then
+        c1.rightWall<-false
+        c2.leftWall<-false
+    if Deltay = -1 then
+        c1.bottomWall<-false
+        c2.topWall<-false
+    if Deltay = 1 then
+        c1.topWall<-false
+        c2.bottomWall<-false
+// TODO: implement the maze type, its generation (task 1) and its automatic resolution (task 2)
+let labirintoVisitato(struttura:cell[,],h:int,w:int)=
+    let mutable k=0
+    let b = [|
+        for i in 0..h-1 do
+            for j in 0..w-1 do
+                if struttura.[i,j].visited=false then
+                    k<-k+1
+    |]
+    if k>0 then true else false
+type maze(w , h ,solve:bool) =
+    member val Struttura:cell[,] = Array2D.create h w (new cell())
+
+    member this.get(x:int,y:int):cell= 
+        Array2D.get this.Struttura x y
+    member this.getByCoordinates(y:int,x:int):cell= 
+        Array2D.get this.Struttura x y
+    member this.generate=
+     //Inizializzo la matrice
+         for i in 0..h-1 do 
+             for j in 0..w-1 do
+                 this.Struttura.[i,j]<-new cell()
+                 this.Struttura.[i,j].x<-j
+                 this.Struttura.[i,j].y<-i
+         this.Struttura.[0,0].visited<-true
+         this.Struttura.[0,0].topWall<-false
+         let mutable i = 0
+         let mutable j = 0
+         let mutable current = getRandomVicino(i,j,h,w,this.Struttura)
+         cancellaMuri(getCell(current),this.Struttura.[0,0])
+         let mutable next = false,new cell()
+         getCell(current).visited<-true
+
+         //Generazione
+         let mutable stack:(bool*cell) list = []
+         stack<-(push(stack,current))
+         while labirintoVisitato(this.Struttura,h,w)=true do
+             if getBool(current)=false then
+                j<-(getCell(current).x)
+                i<-(getCell(current).y)
+                Log.msg "Exploring node: (%d,%d)" j i
+                next <- getRandomVicino(i,j,h,w,this.Struttura)
+                if getBool(next)=false then
+                    getCell(next).visited<-true
+                    //apro i muri
+                    cancellaMuri(getCell(next),getCell(current))
+            
+                    current<-next
+                    stack<-(push(stack,current))
+                else 
+                    if stack.Length>0 then 
+                        current<-stack.Head
+                        stack<-stack.Tail
+             else 
+                 if stack.Length>0 then 
+                     current<-stack.Head
+                     stack<-stack.Tail
+         //Creo un uscita: 
+            //dato che deve essere su un bordo, stabilisco se i o j sono uguali a zero
+         let r = System.Random().Next(0, 2)
+         if r = 0 then
+            i<-0
+            j<-System.Random().Next(1, w)
+         else
+            j<-0
+            i<-System.Random().Next(1, h)
+        
+         if i = 0 then this.Struttura.[i,j].topWall<-false
+         if j = 0 then this.Struttura.[i,j].leftWall<-false
+         if i = h-1 then this.Struttura.[i,j].rightWall<-false
+         if j = w-1 then this.Struttura.[i,j].bottomWall<-false
+         this.Struttura.[i,j].finishLine<-true
+        //reset visited<-false
+         for i in 0..h-1 do 
+            for j in 0..w-1 do
+            this.Struttura.[i,j].visited<- solve
+         this.Struttura.[0,0].visited<-true
+            
+     //La prima cella e' visitata sicuramente
+         
+
+                     
+
+        
---
+++ LabProg2019/PacMan.fs
@@ -0,0 +1,97 @@
+﻿(*
+* LabProg2019 - Progetto di Programmazione a.a. 2019-20
+* Demo1.fs: sample usage of engine
+* (C) 2019 Alvise Spano' @ Universita' Ca' Foscari di Venezia
+*)
+
+module LabProg2019.PacMan
+
+open System
+open Engine
+open Gfx
+open Maze
+open System.Text
+    
+
+[< NoEquality; NoComparison >]
+type state = {
+    player : sprite
+    sprites: sprite[]
+}
+let R = 20 //righe i
+let C = 20  //colonne j
+let GrandezzaCella=5
+let centroCella = GrandezzaCella/2
+
+let genRandomNumbers count =
+    let rnd = System.Random()
+    List.init count (fun _ -> rnd.Next ())
+
+
+let main () =       
+    let engine = new engine (C*GrandezzaCella, R*GrandezzaCella)
+    let maze = new maze(C,R,false)
+    maze.generate
+    let mutable vinto=false
+
+    let my_update (key : ConsoleKeyInfo) (screen : wronly_raster)(inf:info) (st : state) =
+        // move player
+        if vinto=false then
+            let dx, dy =
+                match key.KeyChar with 
+                | 'w' -> 0., float -GrandezzaCella
+                | 's' -> 0., float GrandezzaCella
+                | 'a' -> float -GrandezzaCella, 0.
+                | 'd' -> float GrandezzaCella,0.
+                | _   -> 0., 0.
+            // TODO: check bounds
+            let x = (st.player.x + float dx) 
+            let y = (st.player.y + float dy)
+            try //Controllo che la cella in cui voglio muovermi esiste, e che non esista un muro tra il player e la cella
+                let fixedx = ((int x)-centroCella)/GrandezzaCella
+                let fixedy = ((int y)-centroCella)/GrandezzaCella
+                let currentX = ((int st.player.x)-centroCella)/GrandezzaCella
+                let currentY = ((int st.player.y)-centroCella)/GrandezzaCella
+
+                //Controllo che non ci sia un muro 
+                let currentCella = maze.getByCoordinates(currentX,currentY)
+                if(fixedx-currentX=1 && currentCella.rightWall=false || fixedx-currentX= -1 && currentCella.leftWall=false || fixedy-currentY=1 && currentCella.bottomWall=false || fixedy-currentY= -1 && currentCella.topWall=false) then
+                    let cella = maze.getByCoordinates(fixedx,fixedy) 
+                    cella.visited<-true
+                    Log.msg "cella: (%d,%d), player: (%d,%d)" fixedx fixedy  currentX currentY
+        
+            
+                    let NumeroMappato=fixedx+fixedy*C
+                    if st.sprites.[NumeroMappato].z = 0 then //Solo quando non è mai stato sovrascritto
+                        let immagine = image.cella (GrandezzaCella, GrandezzaCella, Color.Blue,cella)
+                        st.sprites.[NumeroMappato].clear
+                        st.sprites.[NumeroMappato]<-new sprite (immagine, int st.sprites.[NumeroMappato].x, int st.sprites.[NumeroMappato].y, int st.sprites.[NumeroMappato].z+1)
+                        engine.register_sprite st.sprites.[NumeroMappato]
+            
+                    st.player.move_by (dx, dy)
+                    if cella.finishLine=true then
+                        Log.msg "HAI VINTO"
+                        vinto<-true
+            
+            
+            with 
+            | :? System.IndexOutOfRangeException -> printfn "Exception handled.";
+        st, key.KeyChar = 'q'
+
+    let arr =[|
+        for i in 0..R-1 do
+            for j in 0..C-1 do
+                maze.get(i,j).visited<-true
+                let s = engine.create_and_register_sprite (image.cella (GrandezzaCella, GrandezzaCella, Color.Yellow,maze.get(i,j)), (GrandezzaCella*j),(GrandezzaCella*i), 0)
+                yield s
+    |]
+    let player = engine.create_and_register_sprite (image.rectangle (1, 1, pixel.filled Color.Blue), centroCella,centroCella, 0)
+
+    engine.show_fps<-false
+    let st0 = { 
+        player = player
+        sprites=arr
+        }
+
+    engine.loop_on_key my_update st0
+
