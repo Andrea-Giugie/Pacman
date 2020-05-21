78d504651cdb4c9424c9b1b198c83b1daf553267
Andrea-Giugie <bisateo@gmail.com>
2020-05-21 16:42:58 +0200

    Aggiunto trapano

--- LabProg2019/PacMan.fs
+++ LabProg2019/PacMan.fs
@@ -23,13 +23,30 @@ let genRandomNumbers count =
     let rnd = System.Random()
     List.init count (fun _ -> rnd.Next ())
 
-
+let Trapana(cellaVecchia:cell,cellaNuova:cell)=
+    let Deltax = cellaVecchia.x-cellaNuova.x
+    let Deltay = cellaVecchia.y-cellaNuova.y
+    if Deltax = 1 then
+        cellaVecchia.leftWall<-false
+        cellaNuova.rightWall<-false
+    if Deltax = -1 then
+        cellaVecchia.rightWall<-false
+        cellaNuova.leftWall<-false
+    if Deltay = -1 then
+        cellaVecchia.bottomWall<-false
+        cellaNuova.topWall<-false
+    if Deltay = 1 then
+        cellaVecchia.topWall<-false
+        cellaNuova.bottomWall<-false
+   
 let main () =       
     let engine = new engine (C*GrandezzaCella, R*GrandezzaCella)
     let maze = new maze(C,R,false)
     maze.generate
     let mutable vinto=false
     let mutable MovimentoNemici= 0.
+    let mutable TimerTrapano = 0. 
+    let mutable TrapanoAttivo:bool=false
     let arr =[|
         for i in 0..R-1 do
             for j in 0..C-1 do
@@ -52,11 +69,15 @@ let main () =
         if vinto=false then
             match key with
                 |None-> //Devo trovare tutti i nemici e farli muovere a caso
-                        if inf.timer-MovimentoNemici > 0.1 then
+                        if inf.timer-MovimentoNemici > 1. then
                             Log.msg "Devo muovere %f"inf.timer
                             MovimentoNemici<-inf.timer
                             Maze.MuoviNemici(maze,C,R,st,engine,GrandezzaCella)
+                        if inf.timer-TimerTrapano>5. then
+                            TrapanoAttivo<-false
+
                 |Some key->
+                    
                     let dx, dy =
                         match key.KeyChar with 
                         | 'w' -> 0., float -GrandezzaCella
@@ -65,7 +86,12 @@ let main () =
                         | 'd' -> float GrandezzaCella,0.
                         | _   -> 0., 0.
                     // TODO: check bounds
-                    
+                    if key.KeyChar=' ' then
+                        if inf.timer-TimerTrapano>0.0 then// *-************************************modifica in 15 
+                                TimerTrapano<-inf.timer
+                                TrapanoAttivo<-true
+                        
+                        
                     
                     let x = (st.player.x + float dx) 
                     let y = (st.player.y + float dy)
@@ -77,9 +103,16 @@ let main () =
 
                         //Controllo che non ci sia un muro 
                         let currentCella = maze.getByCoordinates(currentX,currentY)
-                        if(fixedx-currentX=1 && currentCella.rightWall=false || fixedx-currentX= -1 && currentCella.leftWall=false || fixedy-currentY=1 && currentCella.bottomWall=false || fixedy-currentY= -1 && currentCella.topWall=false) then
+                        if(fixedx-currentX=1 && currentCella.rightWall=false || fixedx-currentX= -1 && currentCella.leftWall=false || fixedy-currentY=1 && currentCella.bottomWall=false || fixedy-currentY= -1 && currentCella.topWall=false || TrapanoAttivo=true) then
                             let cella = maze.getByCoordinates(fixedx,fixedy) 
-                            cella.visited<-true
+                            if TrapanoAttivo=true then
+                                Trapana(currentCella,cella)
+                                //Modifico i muri della vecchia cella
+                                let NumeroMappato=currentX+currentY*C
+                                let immagine = image.cella (GrandezzaCella, GrandezzaCella, Color.Blue,currentCella)
+                                st.sprites.[NumeroMappato].clear
+                                st.sprites.[NumeroMappato]<-new sprite (immagine, int st.sprites.[NumeroMappato].x, int st.sprites.[NumeroMappato].y, int st.sprites.[NumeroMappato].z+1)
+                                engine.register_sprite st.sprites.[NumeroMappato]
                             Log.msg "cella: (%d,%d), player: (%d,%d)" fixedx fixedy  currentX currentY
                             if cella.coin=true then
                                 score<-score+1
